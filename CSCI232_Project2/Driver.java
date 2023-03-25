454c791c4118e41c35faa0bbc6e55a27c9133580
john-fike <johnfike223@gmail.com>
2023-03-25 15:15:36 -0600

    no clue

---
+++ CSCI232_Project2/Driver.java
@@ -0,0 +1,19 @@
+
+import java.io.File;
+
+/**
+ *
+ * @author yaw
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File dictionaryFile = new File("words.txt");
+        File processingFile = new File("test.txt");
+        DocCheck.spellCheck(processingFile, dictionaryFile);
+        DocCheck.wordCount(processingFile);
+    }
+}
