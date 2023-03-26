82f012b78126f3a187703b447d33746bc400abab
john-fike <johnfike223@gmail.com>
2023-03-26 00:15:58 -0600

    recreate proj2

---
+++ CSCI232_PROJECT2/src/DocCheck.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author yaw
+ */
+public class DocCheck {
+
+    public static void spellCheck(File processingFile, File dictionaryFile) {
+        HashSet<String> dictionary = new HashSet<>();
+
+    }
+
+    public static void wordCount(File processingFile) {
+        HashMap<String, Integer> countPerWord = new HashMap<>();
+        HashMap<Integer, HashSet<String>> wordsPerCount = new HashMap<>();
+
+    }
+}
