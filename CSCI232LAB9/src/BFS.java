729d1a3864b5ab0da0d762b8811ebd3ddca9ffa1
john-fike <johnfike223@gmail.com>
2023-04-24 17:49:46 -0600

    rm rand test comment

--- CSCI232LAB9/src/BFS.java
+++ CSCI232LAB9/src/BFS.java
@@ -25,32 +25,3 @@ public class BFS{
         System.out.println(output);
     }
 }
-
-
-
-//
-//public class MyProgram {
-//    public static void main(String[] args) {
-//        if (args.length != 3) {
-//            System.out.println("Usage: java MyProgram <file> <a> <b>");
-//            return;
-//        }
-//
-//        int a = Integer.parseInt(args[1]);
-//        int b = Integer.parseInt(args[2]);
-//
-//        try {
-//            Scanner scanner = new Scanner(new File(fileName));
-//            int sum = 0;
-//            while (scanner.hasNextInt()) {
-//                int num = scanner.nextInt();
-//                if (num % a == 0 || num % b == 0) {
-//                    sum += num;
-//                }
-//            }
-//            System.out.println("The sum of numbers divisible by " + a + " or " + b + " in file " + fileName + " is " + sum);
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File not found: " + fileName);
-//        }
-//    }
-//}
